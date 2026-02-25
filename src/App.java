9f03e08d2f80763748af21f385e045aa3842d3b0
Nomundari0211 <nbatbold@stu.bau.edu>
2026-02-24 22:23:31 -0500

    first commit

---
+++ src/App.java
@@ -0,0 +1,6 @@
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello, my name is Nomundari Batbold.");
+		System.out.println("Today is my first Java project in VS Code.");
+	}
+}
