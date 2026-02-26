0bfbf3409350a0eef17a0270f336193f889b6439
illiasab <illias.abdv@gmail.com>
2026-02-26 10:26:20 -0500

    Create StudentCheckin layer

---
+++ src/StudetCheckIn.java
@@ -0,0 +1,12 @@
+public class StudetCheckIn {
+    
+    public static void main(String[] args) {
+        String[] names = {"Kaan", "Nilsu", "Rejoy"};
+
+        for (int i = 0; i < names.length; i++){
+            if (names[i].equals("Kaan")) {
+             System.out.println("Kaan is Admited");
+            }
+        }
+    }
+}
