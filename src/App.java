bca220d9896a88475c18ee747ae515d083be7e3d
illiasab <illias.abdv@gmail.com>
2026-03-05 10:15:30 -0500

    Added simple Welcome Layer

--- src/App.java
+++ src/App.java
@@ -1,5 +1,18 @@
+import Constants.Constants;
+import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println(Constants.OnboardingText);
+        Scanner getInput =  new Scanner(System.in);
+        System.out.print(Constants.userOption);
+
+
+        String userSelect = getInput.nextLine();
+        if (userSelect.equals("6")) {
+            return;
+        } else {
+            System.out.println("great choice");
+        }
+
     }
 }
---
+++ src/Constants/Constants.java
@@ -0,0 +1,10 @@
+package Constants;
+
+public final  class Constants {
+    public static final String OnboardingText =
+            "== Campus Connect 2.0 == " +
+                    "\n 1. Create Student \n 2. Register Course \n 3. Calculate GPA \n 4. View Schedule \n 5.  Admin Login \n 6. Exit ";
+    public static final String userOption = " \n your option: ";
+
+    private Constants() {}
+}
