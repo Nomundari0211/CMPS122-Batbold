83fb6cc18a6f578f76e4c03762759937f713fbb0
illiasab <illias.abdv@gmail.com>
2026-03-21 17:38:29 -0400

    fix Onboarding

--- src/Constants/Constants.java
+++ src/Constants/Constants.java
@@ -3,12 +3,27 @@ package Constants;
 public final  class Constants {
     public static final String ONBOARDING_TEXT = """
             == Campus Connect 2.0 ==
-             1. Create Student
-             2. Register Course
-             3. Calculate GPA
-             4. View Schedule
-             5.  Admin Login
-             6. Exit """;
+            1. Create Student
+            2. Add Course
+            3. Add Instructor
+            4. Register for Course
+            5. Calculate GPA
+            6. View Events
+            7. Find Tutor
+            8. Apply for Financial Aid
+            9. Make Payment
+            10. Login
+            11. Build Schedule
+            12. Track Attendance
+            13. Generate Report
+            14. Search
+            15. Sort Data
+            16. View Notifications
+            17. View Stored Data
+            18. Help
+            19. Report Error
+            20. Admin Panel
+            21. Exit""";
     public static final String USER_OPTION = " \n your option: ";
 
     private Constants() {}
