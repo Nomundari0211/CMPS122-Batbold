10c903fe989b83627bdafcd11212043a5a8cf37a
illiasab <illias.abdv@gmail.com>
2026-03-25 22:38:25 -0400

    Fix Ide cosistency

--- src/main/java/RegistrationManager/RegistrationManager.java
+++ src/main/java/RegistrationManager/RegistrationManager.java
@@ -1,3 +1,5 @@
+package RegistrationManager;
+
 import java.util.ArrayList;
 public class RegistrationManager {
     ArrayList<String> registrations; 
@@ -5,15 +7,16 @@ public class RegistrationManager {
     ArrayList<String> courseList; 
 
     public RegistrationManager () {
-        registrations = new ArrayList<String>();
-        studentList = new ArrayList<String>();
-        courseList = new ArrayList<String>();   
+        registrations = new ArrayList<>();
+        studentList = new ArrayList<>();
+        courseList = new ArrayList<>();   
     }
     public RegistrationManager ( ArrayList<String> registrations, ArrayList<String> studentList, ArrayList<String> courseList ) {
         this.registrations = registrations;
         this.studentList = studentList;
         this.courseList = courseList;   
     }
+
     public static void main(String[] args) {
         System.out.println("this is the RegistrationManager class");
         
