a9721df19252755adaac2d117998f0cdf63ef7e1
atakan <aavukat@stu.bau.edu>
2026-03-26 11:31:39 -0400

    Fix Course file location

---
+++ src/main/java/CourseModule/Course.java
@@ -0,0 +1,10 @@
+public class Course  {
+    private String courseName;
+    private String courseCode;
+    private String instructorName;
+    private int credit;
+    private int capacity;
+
+    public Course() {
+    }
+}
