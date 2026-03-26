27cf7000fd48e57d21ac8405716d4239e0f7c7f7
atakan <aavukat@stu.bau.edu>
2026-03-26 11:29:40 -0400

    Create Course.java

---
+++ CourseModule/Course.java
@@ -0,0 +1,11 @@
+package CourseModule;
+public class Course  {
+    private String courseName;
+    private String courseCode;
+    private String instructorName;
+    private int credit;
+    private int capacity;
+
+    public Course() {
+    }
+}
