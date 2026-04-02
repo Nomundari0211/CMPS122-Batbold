1aa818507c1fc594eb064518166784ae6c7ba4cb
rejoy10 <rkc@stu.bau.edu>
2026-04-02 08:58:58 -0400

    new function and getter setter

--- src/main/java/FinancialAid/FinancialAid.java
+++ src/main/java/FinancialAid/FinancialAid.java
@@ -2,11 +2,11 @@ package FinancialAid;
 public class FinancialAid {
 
     // Properties
-    String studentId;
-    double aidAmount;
-    String aidType;
+    private String studentId;
+    private double aidAmount;
+    private String aidType;
 
-    // constructor
+    // Constructors
     public FinancialAid(String studentId, double aidAmount, String aidType) {
         this.studentId = studentId;
         this.aidAmount = aidAmount;
@@ -16,17 +16,72 @@ public class FinancialAid {
     public FinancialAid() {
     }
 
-    // getter
+    // Getters
     public String getStudentId() {
         return this.studentId;
     }
 
-
-    public static void main(String[] args) {
-
+    public double getAidAmount() {
+        return this.aidAmount;
     }
 
-    public void  start() {
-        System.out.print("Welcome to FinancialAid");
+    public String getAidType() {
+        return this.aidType;
+    }
+
+    // Setters
+    public void setStudentId(String studentId) {
+        this.studentId = studentId;
+    }
+
+    public void setAidAmount(double aidAmount) {
+        if (aidAmount < 0) {
+            System.out.println("Aid amount cannot be negative");
+        }
+        else {
+            this.aidAmount = aidAmount;
+        }
+    }
+
+    public void setAidType(String aidType) {
+        this.aidType = aidType;
+    }
+
+    // determining financial aid using student's GPA and household income
+
+    public boolean applyForAid(double gpa, double income) {
+        if (gpa <= 0 || income < 0) {
+            System.out.println("Invalid GPA or income value!");
+            return false;
+        }
+
+        if (gpa >= 3.8 && income < 20000) {
+            this.aidType = "Full Scholarship";
+            this.aidAmount = 20000.00;
+        } else if (gpa >= 2.5 && income < 50000) {
+            this.aidType = "Partial Grant";
+            this.aidAmount = 8000.00;
+        } else if (income < 20000) {
+            this.aidType = "Need-Based Aid";
+            this.aidAmount = 10000.00;
+        } else {
+            System.out.println("Student " + "("+studentId+")"+ " does not qualify for aid.");
+            return false;
+        }
+
+        System.out.println("Aid approved for student " + "("+studentId+")" +
+                "= " + aidType + " ($" + aidAmount + ")");
+        return true;
+    }
+
+    public void start() {
+        System.out.println("Welcome to FinancialAid");
+    }
+
+    public static void main(String[] args) {
+        FinancialAid student = new FinancialAid();
+        student.setStudentId("201245");
+        student.start();
+        student.applyForAid(3.7, 25000); // Expected Full Scholarship
     }
 }
